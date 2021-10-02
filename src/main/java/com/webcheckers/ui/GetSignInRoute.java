814b79697c0ed91377aa1612c1eab117530029f3
Michael Ambrose <ma8540@rit.edu>
2021-10-02 17:03:18 -0400

    getsignin route added, not completed

---
+++ src/main/java/com/webcheckers/ui/GetSignInRoute.java
@@ -0,0 +1,43 @@
+package com.webcheckers.ui;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+import spark.ModelAndView;
+import spark.Request;
+import spark.Response;
+import spark.Route;
+import spark.TemplateEngine;
+import static spark.Spark.halt;
+
+import com.webcheckers.util.Message;
+
+/**
+ * The {@code GET /signin} route handler.
+ *
+ * @author Michael Ambrose
+ */
+
+public class GetSignInRoute {
+
+    private final TemplateEngine templateEngine;
+
+    /**
+     * The constructor for the {@code GET/signin} route handler
+     * 
+     * @param templateEngine
+     */
+
+    public GetSignInRoute(final TemplateEngine templateEngine) {
+        Objects.requireNonNull(templateEngine, "templateEngine must not be null");
+        this.templateEngine = templateEngine;
+    }
+
+    @Override
+    public Object handle(Request request, Response response) {
+
+        return templateEngine.render();
+    }
+}
+

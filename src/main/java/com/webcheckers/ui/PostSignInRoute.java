affbb27ca0c54725800a8dd3d0ad85c6d252bab6
Carlos <csh2830@rit.edu>
2021-10-02 20:41:15 -0400

    Completed first part of signin

--- src/main/java/com/webcheckers/ui/PostSignInRoute.java
+++ src/main/java/com/webcheckers/ui/PostSignInRoute.java
@@ -16,29 +16,30 @@ import static spark.Spark.halt;
  * @author Carlos Hargrove
  */
 public class PostSignInRoute implements Route {
+    //object with the player list and login methods.
     private final PlayerLobby playerLobby;
     private final TemplateEngine templateEngine;
 
-
     /**
-     * The constructor for the @code POST /signin route handler.
+     * The constructor for the POST /signin route handler.
      *
      * @param playerLobby - contains login info. Aids in signing in a user.
      * @param templateEngine - template engine to use for rendering HTML page
      *
-     * @throws NullPointerException
-     *    when the playerLobby or templateEngine parameter is null
      */
     PostSignInRoute(PlayerLobby playerLobby, TemplateEngine templateEngine) {
-        // validation
         Objects.requireNonNull(playerLobby, "playerLobby must not be null");
         Objects.requireNonNull(templateEngine, "templateEngine must not be null");
-        //
         this.playerLobby = playerLobby;
         this.templateEngine = templateEngine;
     }
 
-    //Todo: Finish method.
+    /**
+     * handles the sign in process when the user presses login on the login page.
+     * @param request - request object
+     * @param response - response object
+     * @return - null, returns the user to the home page logged in
+     */
     @Override
     public String handle(Request request, Response response) {
 

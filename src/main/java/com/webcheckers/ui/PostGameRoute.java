ac7dd1cdd8720dcf132f056292ae361ace9d78cc
Carlos <csh2830@rit.edu>
2021-10-04 20:10:49 -0400

    created a shell of a PostGameRoute object

--- src/main/java/com/webcheckers/ui/PostGameRoute.java
+++ src/main/java/com/webcheckers/ui/PostGameRoute.java
@@ -1,4 +1,39 @@
 package com.webcheckers.ui;
 
-public class PostGameRoute {
+import com.webcheckers.appl.PlayerLobby;
+import com.webcheckers.model.Player;
+import spark.*;
+
+import java.util.Objects;
+
+/**
+ * The POST /game route handler.
+ * @author Carlos Hargrove
+ */
+public class PostGameRoute implements Route {
+    private final TemplateEngine templateEngine;
+
+
+    /**
+     * The constructor for the POST /game route handler.
+     *
+     * @param templateEngine - template engine to use for rendering HTML page
+     *
+     * @throws NullPointerException
+     *    when the playerLobby or templateEngine parameter is null
+     */
+    PostGameRoute(TemplateEngine templateEngine) {
+        Objects.requireNonNull(templateEngine, "templateEngine must not be null");
+        this.templateEngine = templateEngine;
+    }
+
+    @Override
+    public String handle(Request request, Response response) {
+
+        final Session session = request.session();
+
+        return null;
+    }
+
 }
+

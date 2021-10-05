4093fe0d4fcc68467bc08c92189a8a9d14146cfd
Michael Ambrose <ma8540@rit.edu>
2021-10-04 20:53:53 -0400

    Added GetGameRoute Component, changed comment in GetSignInRoute

--- src/main/java/com/webcheckers/ui/GetGameRoute.java
+++ src/main/java/com/webcheckers/ui/GetGameRoute.java
@@ -1,4 +1,65 @@
 package com.webcheckers.ui;
 
-public class GetGameRoute {
+import com.webcheckers.util.Message;
+import spark.*;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.logging.Logger;
+
+/**
+ * The {@code GET /game} route handler.
+ *
+ * @author Michael Ambrose
+ */
+
+public class GetGameRoute implements Route{
+    private static final Logger LOG = Logger.getLogger(GetGameRoute.class.getName());
+
+    private final String VIEW_NAME = "game.ftl";
+    private final TemplateEngine templateEngine;
+    private static final Message INVALID_MSG = Message.info("Could not load page");
+
+    
+    /**
+     * The constructor for the {@code GET/game} route handler
+     * 
+     * @param templateEngine
+     */
+
+    public GetGameRoute(final TemplateEngine templateEngine) {
+        Objects.requireNonNull(templateEngine, "templateEngine must not be null");
+        this.templateEngine = templateEngine;
+        LOG.config("GetGameRoute is initialized");
+    }
+
+    /**
+     * Render the WebCheckers Game page
+     * 
+     * @param request
+     *  the HTTP request
+     * @param response
+     *  the HTTP response
+     * 
+     * @return
+     *  the rendered HTML for the Game page
+     */
+
+    @Override
+    public Object handle(Request request, Response response) {
+        Map<String, Object> vm = new HashMap<>();
+
+        Boolean error = request.session().attribute("error");
+
+        if (error != null) {
+            if (error) {
+                vm.put("message", INVALID_MSG);
+            }
+        }
+
+        vm.put("title", "Playing Game");
+
+        return templateEngine.render(new ModelAndView(vm, VIEW_NAME));
+    }
 }

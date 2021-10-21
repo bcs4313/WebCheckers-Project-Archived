afca2a5a24ed5f5c0f93db161a4976833472260e
bcs4313 <bcs4313@g.rit.edu>
2021-10-21 19:30:25 -0400

    Created RuleMaster mainframe that handles all individual rules that are needed to operate within a game.

---
+++ src/main/java/com/webcheckers/model/RuleSystem/RuleMaster.java
@@ -0,0 +1,55 @@
+package com.webcheckers.model.RuleSystem;
+
+import com.webcheckers.model.GameBoard;
+
+import java.util.ArrayList;
+
+/**
+ * RuleMaster is a singular object that handles all individual
+ * rule triggers and actions.
+ */
+public class RuleMaster {
+    ArrayList<Rule> ruleSet; // rules to scroll through on each action
+
+    GameBoard b_before; // stored before state
+    GameBoard b_after; // stored after state
+
+    // basic constructor
+    public RuleMaster()
+    {
+        ruleSet = new ArrayList<>();
+    }
+
+    /**
+     * store board before and after states
+     * to be used by individual rules
+     */
+    public void createBoardTransition(GameBoard before, GameBoard after)
+    {
+        b_before = before;
+        b_after = after;
+    }
+
+    /**
+     * Add a rule to be triggered by the RuleMaster
+     * @param r the rule in question
+     */
+    public void addRule(Rule r)
+    {
+        ruleSet.add(r);
+    }
+
+    /**
+     * Modify the gameboard and do actions by triggering each rule in the ruleset
+     */
+    public void triggerRuleSet()
+    {
+        for(Rule r : ruleSet)
+        {
+            if (r.isTriggered(b_before, b_after))
+            {
+                r.action();
+            }
+        }
+    }
+}

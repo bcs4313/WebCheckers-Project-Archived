b037308470c2711ba265090cf3c0c12833ecd9ac
bcs4313 <bcs4313@g.rit.edu>
2021-10-24 16:30:09 -0400

    Added two methods used to identify a universal singular movement of a checker on the checker board. Created class MovementPair to represent a basic movement of a checker.

--- src/main/java/com/webcheckers/model/RuleSystem/RuleMaster.java
+++ src/main/java/com/webcheckers/model/RuleSystem/RuleMaster.java
@@ -56,6 +56,73 @@ public class RuleMaster {
         }
     }
 
+    // Rule Helper Methods
+
+    /**
+     * Examines entire board to see if a checker has moved, if so,
+     * return it.
+     * @return binary list of new position {y, x} or null
+     */
+    public MovementPair identifyMovement()
+    {
+        for(int y = 0; y < 8; y++)
+        {
+            for(int x = 0; x < 8; x++)
+            {
+                int[] movement = identifyMovementHelper(y, x);
+                if(movement != null)
+                {
+                    MovementPair mp = new MovementPair();
+                    mp.before_y = y;
+                    mp.before_x = x;
+                    mp.after_y = movement[0];
+                    mp.after_x = movement[1];
+                    return mp;
+                }
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Examines a location to see if a checker has moved from that position.
+     * @param start_y initial position
+     * @param start_x end position
+     * @return binary list of new position {y, x}
+     */
+    public int[] identifyMovementHelper(int start_y, int start_x)
+    {
+        // These two arrays run in parallel to identify possible movements
+        int[] yShifts = {-1,-1, -2, 2,  1, 1, 2, 2};
+        int[] xShifts = {-1, 1, -2, 2, -1, 1,-2, 2};
+
+        GameBoard.cells[][] c_before = b_before.getBoard();
+        GameBoard.cells[][] c_after = b_after.getBoard();
+
+        // check to see if original position is empty, if not then the movement
+        // clearly has never occurred
+        if(c_before[start_y][start_x] != GameBoard.cells.E)
+        {
+            return null;
+        }
+
+        for(int i = 0; i < yShifts.length; i++)
+        {
+            int y = yShifts[i];
+            int x = xShifts[i];
+            // first check for a nonempty space
+            if (c_before[y][x] != GameBoard.cells.E) {
+                // now see if the beforeBoard and afterBoard states are different, if so. return
+                if(c_before[y][x] == c_after[y][x])
+                {
+                    return new int[]{y,x};
+                }
+            }
+        }
+
+        return null;
+    }
+
     public void setGameOver(boolean gameOver) {
         isGameOver = gameOver;
     }

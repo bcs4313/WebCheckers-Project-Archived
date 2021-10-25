8933b827872f8a5a0e56ae813a29dbf75bd16e40
bcs4313 <bcs4313@g.rit.edu>
2021-10-25 16:00:33 -0400

    Added basic representations of a Move and a position on a checkerboard. Used to validate a move via a MOVE message.

--- src/main/java/com/webcheckers/model/Move.java
+++ src/main/java/com/webcheckers/model/Move.java
@@ -1,10 +1,33 @@
 package com.webcheckers.model;
 
-public class Move {
+import javax.swing.text.Position;
 
-    public Move(String inputString, Move move){
-        System.out.println(inputString);
+/**
+ * Representation of a movement from one position to another
+ * @author Cody Smith (bcs4313@rit.edu)
+ */
+public class Move {
+    // positions that represent a movement
+    private Position previousPos;
+    private Position newPos;
+
+    /**
+     * Basic constructor for movement representation
+     * @param previousPos position of checker target before movement
+     * @param newPos position of checker target after movement
+     */
+    public Move(Position previousPos, Position newPos){
+        this.previousPos = previousPos;
+        this.newPos = newPos;
     }
 
+    public Position getStart()
+    {
+        return previousPos;
+    }
 
+    public Position getEnd()
+    {
+        return newPos;
+    }
 }

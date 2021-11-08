652ff3b20c7dd653c9541f9c7480f4cb02ffa113
bcs4313 <bcs4313@g.rit.edu>
2021-11-08 18:08:10 -0500

    - Board now forces players to jump for their first movement if a jump is available. This is applied using the new Rule InitJumpRule. - Added special case for InitJumpRule to force chain jumps to be completed, mustJump() wasn't working... - Removed function mustJump(), it wasn't serving its purpose properly. - Restricted ability of player to jump after a basic movement (bugged). - Restricted ability to end turn in the middle of a chain (bugged). - Moved flip board function to Rule abstraction. This follows the "Don't repeat yourself" mindset.

--- src/main/java/com/webcheckers/model/RuleSystem/Chainer.java
+++ src/main/java/com/webcheckers/model/RuleSystem/Chainer.java
@@ -15,6 +15,7 @@ public class Chainer {
     // chain
     ArrayList<Position> jumpChains;
     RuleMaster master; // used to reference GameBoard state
+    GameBoard.cells[][] initState; // initial state of board per turn
 
     /**
      * Basic chaining class to track
@@ -45,32 +46,28 @@ public class Chainer {
         }
     }
 
+    public int getLength() { return jumpChains.size(); }
+
+    /**
+     * Get latest position of jump location
+     * @return Position object
+     */
+    public Position head()
+    {
+        if(jumpChains.size() != 0) {
+            return jumpChains.get(jumpChains.size() - 1);
+        }
+        else
+        {
+            return null;
+        }
+    }
+
     /**
      * Clear player of jump chain.
      */
     public void clearJumps()
     {
         jumpChains.clear();
-    }
-
-    /**
-     * Is the player currently inside of a jump chain,
-     * and must they jump again?
-     * @return if a player must jump again
-     */
-    public boolean mustJump()
-    {
-        if(jumpChains.size() >= 1) // if we know they are in a chain
-        {
-            // now we must check to see if a jump must occur
-            BackwardJumpRule brule = new BackwardJumpRule(master);
-            ForwardJumpRule frule = new ForwardJumpRule(master);
-
-            GameBoard.cells[][] b_before = master.getB_Before();
-            GameBoard.cells[][] b_after = master.getB_After();
-
-            return !(brule.isTriggered(b_before, b_after) && frule.isTriggered(b_before, b_after));
-        }
-        return false;
     }
 }
--- src/main/java/com/webcheckers/model/RuleSystem/ForwardJumpRule.java
+++ src/main/java/com/webcheckers/model/RuleSystem/ForwardJumpRule.java
@@ -94,35 +94,4 @@ public class ForwardJumpRule extends Rule {
     public void action() {
         master.invalidForwardJump = true;
     }
-
-    /**
-     * Flips a matrix. Annoying that this is needed
-     * but it makes everything else work.
-     * @param b matrix to flip
-     */
-    private GameBoard.cells[][] flipBoard(GameBoard.cells[][] b)
-    {
-
-        GameBoard.cells[][] boardFlipped = new GameBoard.cells[8][8];
-
-        // copy a board, raw
-        for(int i = 0; i <= 7; i++) {
-            // for each piece
-            for(int j = 0; j < boardFlipped[i].length; j++) {
-                // if the piece is red, change to white
-                boardFlipped[i][j] = b[i][j];
-            }
-        }
-
-        // for each row in the bottom half of the board
-        for(int i = 0; i <= 7; i++) {
-            // for each piece
-            for(int j = 0; j < boardFlipped[i].length; j++) {
-                // if the piece is red, change to white
-                boardFlipped[i][j] = b[7 - i][7 - j];
-            }
-        }
-
-        return boardFlipped;
-    }
 }
